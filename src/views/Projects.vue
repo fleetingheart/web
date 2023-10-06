1ee5062303868e1b234146727fe0f6593f883cfa
TibixDev <fuloptibi03@gmail.com>
2023-10-06 22:31:00 +0300

    - Swap Codeberg Icon - Swap Flatpak Icon - Improve responsivity on smaller devices - Improve A11Y on certain icons - Add forum button to the homepage

--- src/views/Projects.vue
+++ src/views/Projects.vue
@@ -9,7 +9,7 @@
                     <div class="download flex flex-row flex-wrap gap-4">
                         <a href="https://github.com/fleetingheart/ksre/releases/download/v1.3.0/KSRE-win.zip"
                             class="fhs-button flex flex-row items-center gap-1">
-                            <svg width="32" height="32" viewBox="0 0 24 24">
+                            <svg alt="Windows Logo" width="32" height="32" viewBox="0 0 24 24">
                                 <path fill="#ffebbe"
                                     d="m3 5.479l7.377-1.016v7.127H3V5.48zm0 13.042l7.377 1.017v-7.04H3v6.023zm8.188 1.125L21 21v-8.502h-9.812v7.148zm0-15.292v7.236H21V3l-9.812 1.354z" />
                             </svg>
@@ -17,19 +17,19 @@
                         </a>
                         <a href="https://github.com/fleetingheart/ksre/releases/download/v1.3.0/KSRE-linux.tar.bz2"
                             class="fhs-button flex flex-row items-center gap-1">
-                            <svg width="32" height="32" viewBox="0 0 24 24">
+                            <svg alt="Linux Logo" width="32" height="32" viewBox="0 0 24 24">
                                 <path fill="#ffebbe"
                                     d="M14.62 8.35c-.42.28-1.75 1.04-1.95 1.19c-.39.31-.75.29-1.14-.01c-.2-.16-1.53-.92-1.95-1.19c-.48-.31-.45-.7.08-.92c1.64-.69 3.28-.64 4.91.03c.49.21.51.6.05.9m7.22 7.28c-.93-2.09-2.2-3.99-3.84-5.66a4.31 4.31 0 0 1-1.06-1.88c-.1-.33-.17-.67-.24-1.01c-.2-.88-.29-1.78-.7-2.61c-.73-1.58-2-2.4-3.84-2.47c-1.81.05-3.16.81-3.95 2.4c-.21.43-.36.88-.46 1.34c-.17.76-.32 1.55-.5 2.32c-.15.65-.45 1.21-.96 1.71c-1.61 1.57-2.9 3.37-3.88 5.35c-.14.29-.28.58-.37.88c-.19.66.29 1.12.99.96c.44-.09.88-.18 1.3-.31c.41-.15.57-.05.67.35c.65 2.15 2.07 3.66 4.24 4.5c4.12 1.56 8.93-.66 9.97-4.58c.07-.27.17-.37.47-.27c.46.14.93.24 1.4.35c.49.09.85-.16.92-.64c.03-.26-.06-.49-.16-.73" />
                             </svg>
                             Linux
                         </a>
                         <a href="https://flathub.org/apps/sh.fhs.KatawaShoujoReEngineered" class="fhs-button flex flex-row items-center gap-1">
-                            <img src="/img/flathub.png" alt="Flathub Logo" class="w-6 h-6 pr-1">
+                            <img src="/img/flatpak.svg" alt="Flathub Logo" class="w-6 h-6 pr-1">
                             Flathub (Linux)
                         </a>
                         <a href="https://github.com/fleetingheart/ksre/releases/download/v1.3.0/KSRE-mac.zip"
                             class="fhs-button flex flex-row items-center gap-1">
-                            <svg width="32" height="32" viewBox="0 0 24 24">
+                            <svg alt="Desktop Logo" width="32" height="32" viewBox="0 0 24 24">
                                 <path fill="#ffebbe"
                                     d="M8 21v-1l2-2H4q-.825 0-1.412-.587Q2 16.825 2 16V5q0-.825.588-1.413Q3.175 3 4 3h16q.825 0 1.413.587Q22 4.175 22 5v11q0 .825-.587 1.413Q20.825 18 20 18h-6l2 2v1Zm-4-8h16V5H4Zm0 0V5v8Z" />
                             </svg>
@@ -37,7 +37,7 @@
                         </a>
                         <a href="https://github.com/fleetingheart/ksre/releases/download/v1.3.0/com.fhs.ksre-release.apk"
                             class="fhs-button flex flex-row items-center gap-1">
-                            <svg width="32" height="32" viewBox="0 0 24 24">
+                            <svg alt="Android Logo" width="32" height="32" viewBox="0 0 24 24">
                                 <path fill="#ffebbe"
                                     d="M1 18q.225-2.675 1.638-4.925Q4.05 10.825 6.4 9.5L4.55 6.3q-.15-.225-.075-.475q.075-.25.325-.375q.2-.125.45-.05t.4.3L7.5 8.9Q9.65 8 12 8t4.5.9l1.85-3.2q.15-.225.4-.3q.25-.075.45.05q.25.125.325.375q.075.25-.075.475L17.6 9.5q2.35 1.325 3.763 3.575Q22.775 15.325 23 18Zm6-2.75q.525 0 .888-.363q.362-.362.362-.887t-.362-.887Q7.525 12.75 7 12.75t-.887.363q-.363.362-.363.887t.363.887q.362.363.887.363Zm10 0q.525 0 .888-.363q.362-.362.362-.887t-.362-.887q-.363-.363-.888-.363t-.887.363q-.363.362-.363.887t.363.887q.362.363.887.363Z" />
                             </svg>
@@ -45,18 +45,18 @@
                         </a>
                         <a href="https://github.com/fleetingheart/ksre/releases/download/v1.3.0/KSRE-ios.ipa"
                             class="fhs-button flex flex-row items-center gap-1">
-                            <svg width="32" height="32" viewBox="0 0 24 24">
+                            <svg alt="iOS Logo" width="32" height="32" viewBox="0 0 24 24">
                                 <path fill="#ffebbe"
                                     d="M17.05 20.28c-.98.95-2.05.8-3.08.35c-1.09-.46-2.09-.48-3.24 0c-1.44.62-2.2.44-3.06-.35C2.79 15.25 3.51 7.59 9.05 7.31c1.35.07 2.29.74 3.08.8c1.18-.24 2.31-.93 3.57-.84c1.51.12 2.65.72 3.4 1.8c-3.12 1.87-2.38 5.98.48 7.13c-.57 1.5-1.31 2.99-2.54 4.09l.01-.01zM12.03 7.25c-.15-2.23 1.66-4.07 3.74-4.25c.29 2.58-2.34 4.5-3.74 4.25z" />
                             </svg>
                             iOS
                         </a>
                         <a href="https://codeberg.org/fhs" class="fhs-button flex flex-row items-center gap-1">
-                            <img src="/img/codeberg.svg" alt="Codeberg Logo" class="w-6 h-6 pr-1">
+                            <img src="/img/codeberg.png" alt="Codeberg Logo" class="w-7 h-7">
                             Codeberg
                         </a>
                         <a href="https://github.com/fleetingheart" class="fhs-button flex flex-row items-center gap-1">
-                            <svg width="32" height="32" viewBox="0 0 24 24">
+                            <svg alt="GitHub Logo" width="32" height="32" viewBox="0 0 24 24">
                                 <path fill="#ffebbe"
                                     d="M12 2A10 10 0 0 0 2 12c0 4.42 2.87 8.17 6.84 9.5c.5.08.66-.23.66-.5v-1.69c-2.77.6-3.36-1.34-3.36-1.34c-.46-1.16-1.11-1.47-1.11-1.47c-.91-.62.07-.6.07-.6c1 .07 1.53 1.03 1.53 1.03c.87 1.52 2.34 1.07 2.91.83c.09-.65.35-1.09.63-1.34c-2.22-.25-4.55-1.11-4.55-4.92c0-1.11.38-2 1.03-2.71c-.1-.25-.45-1.29.1-2.64c0 0 .84-.27 2.75 1.02c.79-.22 1.65-.33 2.5-.33c.85 0 1.71.11 2.5.33c1.91-1.29 2.75-1.02 2.75-1.02c.55 1.35.2 2.39.1 2.64c.65.71 1.03 1.6 1.03 2.71c0 3.82-2.34 4.66-4.57 4.91c.36.31.69.92.69 1.85V21c0 .27.16.59.67.5C19.14 20.16 22 16.42 22 12A10 10 0 0 0 12 2Z" />
                             </svg>
