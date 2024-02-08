c9e3ad02667d02d7e363a789dbfe9715f010f622
TibixDev <fuloptibi03@gmail.com>
2024-02-09 01:24:56 +0200

    feat: Add F-Droid to download links

--- src/data/KSRE.js
+++ src/data/KSRE.js
@@ -9,6 +9,7 @@ export default {
         flathub: "https://flathub.org/apps/sh.fhs.KatawaShoujoReEngineered",
         mac: `https://github.com/fleetingheart/ksre/releases/download/${KSRE_VERSION}/KSRE-mac.zip`,
         android: `https://github.com/fleetingheart/ksre/releases/download/${KSRE_VERSION}/com.fhs.ksre-release.apk`,
+        fdroid: `https://f-droid.org/en/packages/com.fhs.ksre`,
         ios: `https://github.com/fleetingheart/ksre/releases/download/${KSRE_VERSION}/KSRE-ios.ipa`
     }
 }
