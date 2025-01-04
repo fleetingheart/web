dc9b218f4c54eb0e0ed73335a1ae1f13298f92af
IkariyaBiorante <ikariyabiorante@noreply.codeberg.org>
2025-01-04 16:41:33 +0000

    chore: bump version

--- src/data/KSRE.js
+++ src/data/KSRE.js
@@ -1,4 +1,4 @@
-const KSRE_VERSION = 'v1.4.9';
+const KSRE_VERSION = 'v2.0.0';
 
 export default {
     version: KSRE_VERSION,
@@ -8,7 +8,7 @@ export default {
         linux: `https://github.com/fleetingheart/ksre/releases/download/${KSRE_VERSION}/KSRE-linux.tar.bz2`,
         flathub: "https://flathub.org/apps/sh.fhs.KatawaShoujoReEngineered",
         mac: `https://github.com/fleetingheart/ksre/releases/download/${KSRE_VERSION}/KSRE-mac.zip`,
-        android: `https://github.com/fleetingheart/ksre/releases/download/${KSRE_VERSION}/com.fhs.ksre-release.apk`,
+        android: `https://github.com/fleetingheart/ksre/releases/download/${KSRE_VERSION}/sh.fhs.ksre-release.apk`,
         fdroid: `https://f-droid.org/en/packages/com.fhs.ksre`,
         ios: `https://github.com/fleetingheart/ksre/releases/download/${KSRE_VERSION}/KSRE-ios.ipa`
     }
