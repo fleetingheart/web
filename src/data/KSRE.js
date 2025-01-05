0ba3c4c93e88281d5e0ece13506caa26c81e88d2
Vladimir Hodakov <vladimir@hodakov.me>
2025-01-05 06:53:03 +0400

    Update Flathub link

--- src/data/KSRE.js
+++ src/data/KSRE.js
@@ -6,7 +6,7 @@ export default {
         web: "https://play.fhs.sh/",
         windows: `https://github.com/fleetingheart/ksre/releases/download/${KSRE_VERSION}/KSRE-win.zip`,
         linux: `https://github.com/fleetingheart/ksre/releases/download/${KSRE_VERSION}/KSRE-linux.tar.bz2`,
-        flathub: "https://flathub.org/apps/sh.fhs.KatawaShoujoReEngineered",
+        flathub: "https://flathub.org/apps/sh.fhs.ksre",
         mac: `https://github.com/fleetingheart/ksre/releases/download/${KSRE_VERSION}/KSRE-mac.zip`,
         android: `https://github.com/fleetingheart/ksre/releases/download/${KSRE_VERSION}/sh.fhs.ksre-release.apk`,
         fdroid: `https://f-droid.org/en/packages/com.fhs.ksre`,
