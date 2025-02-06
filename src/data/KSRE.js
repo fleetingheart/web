43f5e0597475ad06e49388cd907a26cbc0012af3
Niomesan <nijo@fhs.sh>
2025-02-06 11:13:09 +0100

    Moved remaining URLs to the rest inside the object.

--- src/data/KSRE.js
+++ src/data/KSRE.js
@@ -9,7 +9,9 @@ export default {
         flathub: "https://flathub.org/apps/sh.fhs.ksre",
         mac: `https://github.com/fleetingheart/ksre/releases/download/${KSRE_VERSION}/KSRE-mac.zip`,
         android: `https://github.com/fleetingheart/ksre/releases/download/${KSRE_VERSION}/sh.fhs.ksre-release.apk`,
-        fdroid: `https://f-droid.org/en/packages/sh.fhs.ksre/`,
-        ios: `https://github.com/fleetingheart/ksre/releases/download/${KSRE_VERSION}/KSRE-ios.ipa`
+        ios: `https://github.com/fleetingheart/ksre/releases/download/${KSRE_VERSION}/KSRE-ios.ipa`,
+        fdroid: 'https://f-droid.org/en/packages/sh.fhs.ksre/',
+        aur: 'https://aur.archlinux.org/packages/katawa-shoujo-reengineered-bin',
+        nix_os: 'https://search.nixos.org/packages?channel=24.11&from=0&size=50&sort=relevance&type=packages&query=katawa-shoujo-re-engineered'
     }
 }
